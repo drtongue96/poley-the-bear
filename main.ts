6f95bee820ef200c1de51b7586cfae0d8c515518
drtongue96 <73095936+drtongue96@users.noreply.github.com>
2021-11-30 12:41:54 -0600

    minor update

--- main.ts
+++ main.ts
@@ -1,16 +1,19 @@
 function setupLevel (lvl: number) {
-	
+    tiles.setTilemap(tilemap`level1`)
 }
 function startGame () {
     setupLevel(currentLevel)
 }
 function initializeGame () {
-	
+    effects.blizzard.startScreenEffect()
 }
 function doCutScene (scene2: number) {
     if (currentLevel == 0) {
     	
     }
+}
+function saveGame () {
+	
 }
 /**
  * Scene 1: Cut scene with glacier
@@ -19,11 +22,12 @@ function doCutScene (scene2: number) {
  * 
  * Scene 3: Stealth: Collect Garbage before you die
  * 
- * Scene 4:
+ * Scene 4: Girl collects Garbage to create wind farms
  */
 let currentLevel = 0
-let hero = sprites.create(assets.image`sprPoley`, SpriteKind.Player)
+let hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
 controller.moveSprite(hero)
+scene.cameraFollowSprite(hero)
 currentLevel = 0
 initializeGame()
 startGame()
