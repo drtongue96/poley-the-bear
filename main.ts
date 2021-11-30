eea94cbf0789160522f2c1a69e49d54a458df5c8
drtongue96 <73095936+drtongue96@users.noreply.github.com>
2021-11-30 11:09:27 -0600

    Added some starter functions

--- main.ts
+++ main.ts
@@ -1,2 +1,29 @@
+function setupLevel (lvl: number) {
+	
+}
+function startGame () {
+    setupLevel(currentLevel)
+}
+function initializeGame () {
+	
+}
+function doCutScene (scene2: number) {
+    if (currentLevel == 0) {
+    	
+    }
+}
+/**
+ * Scene 1: Cut scene with glacier
+ * 
+ * Scene 2: Escape the water
+ * 
+ * Scene 3: Stealth: Collect Garbage before you die
+ * 
+ * Scene 4:
+ */
+let currentLevel = 0
 let hero = sprites.create(assets.image`sprPoley`, SpriteKind.Player)
 controller.moveSprite(hero)
+currentLevel = 0
+initializeGame()
+startGame()
