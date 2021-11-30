008819b64ec2e9f0fbe254e110f1093ebdd0a677
drtongue96 <73095936+drtongue96@users.noreply.github.com>
2021-11-30 16:27:01 -0600

    more setup

--- main.ts
+++ main.ts
@@ -1,20 +1,3 @@
-function setupLevel (lvl: number) {
-    tiles.setTilemap(tilemap`level1`)
-}
-function startGame () {
-    setupLevel(currentLevel)
-}
-function initializeGame () {
-    effects.blizzard.startScreenEffect()
-}
-function doCutScene (scene2: number) {
-    if (currentLevel == 0) {
-    	
-    }
-}
-function saveGame () {
-	
-}
 /**
  * Scene 1: Cut scene with glacier
  * 
@@ -24,10 +7,48 @@ function saveGame () {
  * 
  * Scene 4: Girl collects Garbage to create wind farms
  */
+function setupLevel (lvl: number) {
+    music.stopAllSounds()
+    tiles.loadMap(tiles.createMap(tilemap`level1`))
+}
+function playSound (sound: string) {
+	
+}
+function playMusic (song: string) {
+	
+}
+function initializePlayer () {
+	
+}
+function startGame () {
+    doCutScene(1)
+    setupLevel(currentLevel)
+}
+function initializeGame () {
+    effects.blizzard.startScreenEffect()
+}
+function doCutScene (scene2: number) {
+    if (currentLevel == 0) {
+    	
+    }
+    if (scene2 == 1) {
+        tiles.loadMap(tiles.createMap(tilemap`tmCutscene1`))
+    }
+    story.startCutscene(function () {
+        timer.after(2000, function () {
+            game.splash("Poley The Bear")
+        })
+    })
+}
+function saveGame () {
+	
+}
 let currentLevel = 0
+let debugMode = false
 let hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
 controller.moveSprite(hero)
 scene.cameraFollowSprite(hero)
 currentLevel = 0
 initializeGame()
+initializePlayer()
 startGame()
