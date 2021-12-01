ca3fb7dcacf5b1f81654eaf1937018045653ef3b
drtongue96 <73095936+drtongue96@users.noreply.github.com>
2021-11-30 19:37:56 -0600

    added an npc and sprite sight extension (not working)

--- images.g.ts
+++ images.g.ts
@@ -41,6 +41,25 @@ namespace myImages {
 . . 1 1 . d d . . 1 1 . d d . . 
 . . e e . e e . . e e . e e . . 
 `;
+            case "image3":
+            case "sprVillager1":return img`
+. . . . f f f f . . . 
+. . f f f f f f f f . 
+. f f f f f f c f f f 
+f f f f f f c c f f f 
+f f f c f f f f f f f 
+c c c f f f e e f f c 
+f f f f f e e f f c c 
+f f f b f e e f b f f 
+. f 4 1 f 4 4 f 1 4 f 
+. f e 4 4 4 4 4 4 e f 
+. f f f e e e e f f f 
+f e f b 7 7 7 7 b f e 
+e 4 f 7 7 7 7 7 7 f 4 
+e e f 6 6 6 6 6 6 f e 
+. . . f f f f f f . . 
+. . . f f . . f f . . 
+`;
         }
         return null;
     })
--- main.ts
+++ main.ts
@@ -1,15 +1,22 @@
-/**
- * Scene 1: Cut scene with glacier
- * 
- * Scene 2: Escape the water
- * 
- * Scene 3: Stealth: Collect Garbage before you die
- * 
- * Scene 4: Girl collects Garbage to create wind farms
- */
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+}
+// Scene 1: Cut scene with glacier
+// 
+// Scene 2: Escape the water
+// 
+// Scene 3: Stealth: Collect Garbage before you die
+// 
+// Scene 4: Girl collects Garbage to create wind farms
 function setupLevel (lvl: number) {
     music.stopAllSounds()
     tiles.loadMap(tiles.createMap(tilemap`level1`))
+    effects.blizzard.startScreenEffect()
+    tiles.placeOnTile(hero, tiles.getTileLocation(7, 0))
+    for (let value of tiles.getTilesByType(tiles.util.object1)) {
+        myNPC = sprites.create(assets.image`sprVillager1`, SpriteKind.NPC)
+        tiles.placeOnTile(myNPC, value)
+    }
 }
 function playSound (sound: string) {
 	
@@ -18,37 +25,47 @@ function playMusic (song: string) {
 	
 }
 function initializePlayer () {
-	
+    hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
+    controller.moveSprite(hero)
+    scene.cameraFollowSprite(hero)
 }
 function startGame () {
-    doCutScene(1)
     setupLevel(currentLevel)
 }
 function initializeGame () {
-    effects.blizzard.startScreenEffect()
+    currentLevel = 0
 }
 function doCutScene (scene2: number) {
-    if (currentLevel == 0) {
-    	
-    }
-    if (scene2 == 1) {
-        tiles.loadMap(tiles.createMap(tilemap`tmCutscene1`))
-    }
     story.startCutscene(function () {
-        timer.after(2000, function () {
-            game.splash("Poley The Bear")
-        })
+        controller.moveSprite(hero, 0, 0)
+        hero.setFlag(SpriteFlag.Invisible, true)
+        if (scene2 == 1) {
+            tiles.loadMap(tiles.createMap(tilemap`tmCutscene1`))
+        }
+    })
+    timer.after(4000, function () {
+        game.splash("Poley The Bear")
     })
 }
 function saveGame () {
 	
 }
 let currentLevel = 0
+let myNPC: Sprite = null
+let hero: Sprite = null
 let debugMode = false
-let hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
-controller.moveSprite(hero)
-scene.cameraFollowSprite(hero)
-currentLevel = 0
 initializeGame()
 initializePlayer()
 startGame()
+game.onUpdateInterval(500, function () {
+    if (sight.isInSight(
+    myNPC,
+    hero,
+    40,
+    true
+    )) {
+        myNPC.sayText("i see you")
+    } else {
+        myNPC.sayText("Nothing")
+    }
+})
