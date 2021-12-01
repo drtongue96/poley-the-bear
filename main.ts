a7076b4dc98a31fbefa32ce727959c2d99775e94
drtongue96 <73095936+drtongue96@users.noreply.github.com>
2021-11-30 20:22:07 -0600

    added some music

--- main.ts
+++ main.ts
@@ -22,7 +22,62 @@ function playSound (sound: string) {
 	
 }
 function playMusic (song: string) {
-	
+    if (song == "poleydies") {
+        timer.background(function () {
+            music.setTempo(160)
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(440, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.playTone(494, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(440, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Double))
+        })
+        timer.background(function () {
+            music.playTone(247, music.beat(BeatFraction.Double))
+            music.playTone(220, music.beat(BeatFraction.Double))
+        })
+    }
+    if (song == "something") {
+        timer.background(function () {
+            music.setTempo(160)
+            music.playTone(494, music.beat(BeatFraction.Whole))
+            music.playTone(440, music.beat(BeatFraction.Whole))
+            music.playTone(659, music.beat(BeatFraction.Whole))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Quarter))
+            music.playTone(247, music.beat(BeatFraction.Quarter))
+            music.playTone(330, music.beat(BeatFraction.Half))
+            music.playTone(220, music.beat(BeatFraction.Whole))
+            music.playTone(294, music.beat(BeatFraction.Whole))
+            music.rest(music.beat(BeatFraction.Whole))
+        })
+        timer.background(function () {
+            music.playTone(165, music.beat(BeatFraction.Whole))
+            music.playTone(147, music.beat(BeatFraction.Half))
+            music.rest(music.beat(BeatFraction.Quarter))
+            music.playTone(165, music.beat(BeatFraction.Quarter))
+            music.playTone(247, music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Quarter))
+            music.rest(music.beat(BeatFraction.Half))
+            music.playTone(247, music.beat(BeatFraction.Whole))
+            music.playTone(220, music.beat(BeatFraction.Half))
+            music.rest(music.beat(BeatFraction.Half))
+            music.playTone(165, music.beat(BeatFraction.Whole))
+            music.playTone(147, music.beat(BeatFraction.Whole))
+            music.rest(music.beat(BeatFraction.Whole))
+        })
+        timer.background(function () {
+            music.rest(music.beat(BeatFraction.Breve))
+            music.playTone(440, music.beat(BeatFraction.Quarter))
+            music.playTone(370, music.beat(BeatFraction.Quarter))
+            music.playTone(494, music.beat(BeatFraction.Half))
+            music.playTone(330, music.beat(BeatFraction.Whole))
+            music.playTone(370, music.beat(BeatFraction.Whole))
+            music.rest(music.beat(BeatFraction.Whole))
+        })
+    }
 }
 function initializePlayer () {
     hero = sprites.create(assets.image`myImage`, SpriteKind.Player)
@@ -61,7 +116,7 @@ game.onUpdateInterval(500, function () {
     if (sight.isInSight(
     myNPC,
     hero,
-    40,
+    50,
     false
     )) {
         myNPC.sayText("i see you")
